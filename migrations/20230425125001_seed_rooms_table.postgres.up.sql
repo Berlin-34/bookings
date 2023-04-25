ac9f7863e1bf84294286a5f49cbeb7207bc16da8
Poojan Patel <poojanpatel34@gmail.com>
2023-04-25 18:23:45 +0530

    Added reservation in generals quarters page

---
+++ migrations/20230425125001_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name, created_at, updated_at)
+VALUES ('General''s Quarters', '2023-04-23 14:49:10.000000', '2023-04-23 14:49:12.000000'),
+       ('Major''s Suite', '2023-04-23 15:37:18.000000', '2023-04-23 15:37:20.000000');
---
+++ migrations/20230425125220_seed_restritions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name, created_at, updated_at)
+VALUES ('Reservation', '2023-04-23 15:00:47.000000', '2023-04-23 15:00:48.000000'),
+       ('Owner Block', '2023-04-23 15:31:20.000000', '2023-04-23 15:31:22.000000');
